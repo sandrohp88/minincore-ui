a172f7e324cf76b591cd90bd1fdd1d8cc41cc2dc
Sandro Herrera Pallares <sandrohp88@gmail.com>
2022-07-09 19:38:55 -0700

    Add Header to all pages

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,28 @@
-import "../styles/globals.css";
+import createCache from "@emotion/cache";
+import { CacheProvider } from "@emotion/react";
+import CssBaseline from "@mui/material/CssBaseline";
+import Head from "next/head";
 import PropTypes from "prop-types";
+import Header from "../components/Header";
 
 const propTypes = {
   Component: PropTypes.elementType.isRequired,
   pageProps: PropTypes.object.isRequired, // eslint-disable-line
 };
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <CacheProvider value={createCache({ key: "css" })}>
+      <Head>
+        <title>Nerd Pool</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
+      <CssBaseline />
+      <Header {...pageProps} />
+      <Component {...pageProps} />
+    </CacheProvider>
+  );
 }
+
 MyApp.propTypes = propTypes;
 export default MyApp;
