30616fc88d6ccbc3f2401c3e74e08e32fdd1d177
Sandro Herrera Pallares <sandrohp88@gmail.com>
2022-07-09 19:32:17 -0700

    Blank project setup

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import PropTypes from "prop-types";
 
+const propTypes = {
+  Component: PropTypes.elementType.isRequired,
+  pageProps: PropTypes.object.isRequired, // eslint-disable-line
+};
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
-
-export default MyApp
+MyApp.propTypes = propTypes;
+export default MyApp;
