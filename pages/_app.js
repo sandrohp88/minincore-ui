c09ab028de46b8f82c219e1d005210ec0c496dc7
Sandro Herrera Pallares <sandrohp88@gmail.com>
2022-07-09 19:40:10 -0700

    Add ThemeProvider

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,10 @@
 import createCache from "@emotion/cache";
 import { CacheProvider } from "@emotion/react";
 import CssBaseline from "@mui/material/CssBaseline";
+import { ThemeProvider } from "@mui/material/styles";
 import Head from "next/head";
 import PropTypes from "prop-types";
+import { theme } from "../styles/theme";
 import Header from "../components/Header";
 
 const propTypes = {
@@ -13,13 +15,15 @@ const propTypes = {
 function MyApp({ Component, pageProps }) {
   return (
     <CacheProvider value={createCache({ key: "css" })}>
-      <Head>
-        <title>Nerd Pool</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      </Head>
-      <CssBaseline />
-      <Header {...pageProps} />
-      <Component {...pageProps} />
+      <ThemeProvider theme={theme}>
+        <Head>
+          <title>Nerd Pool</title>
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        </Head>
+        <CssBaseline />
+        <Header {...pageProps} />
+        <Component {...pageProps} />
+      </ThemeProvider>
     </CacheProvider>
   );
 }
